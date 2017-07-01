df1e980b12731bccdabd35cdb7f9102e4f931dcb
Tim Morgan <tim@squareup.com>
2017-06-30 20:22:40 -0700

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,28 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Ranked
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.template_engine     :slim
+      g.test_framework      :rspec, fixture: true, views: false
+      g.integration_tool    :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+  end
+end

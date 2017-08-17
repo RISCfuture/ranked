b773d170d4a67c98541731b2aa4a3a9c55d8c00b
Tim Morgan <github@timothymorgan.info>
2017-08-17 03:10:06 -0700

    Fix bug caused by integer limitations of JS

--- app/javascript/packs/views/stacks/encoder.js
+++ app/javascript/packs/views/stacks/encoder.js
@@ -1,14 +1,15 @@
 import _ from 'lodash'
+import bigInt from 'big-integer'
 
 export default {
   encode(results) {
     let resultsStr = _.map(results, r => r.toString()).join('')
-    let resultsNum = Number.parseInt(resultsStr, 4)
+    let resultsNum = bigInt(resultsStr, 4)
     return resultsNum.toString(36)
   },
 
   decode(param) {
-    let resultsNum = Number.parseInt(param, 36)
+    let resultsNum = bigInt(param, 36)
     let resultsStr = resultsNum.toString(4)
     let resultsArray = resultsStr.split('')
     return _.map(resultsArray, r => Number.parseInt(r))

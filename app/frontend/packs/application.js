d80c53d2b5deaf38fe2ab48bf3e41e9e37a34e0b
Tim Morgan <github@timothymorgan.info>
2018-02-05 21:53:30 -0800

    Add precompiled root template

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,20 +1,18 @@
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual
-// application logic in a relevant structure within app/javascript and only use
-// these pack files to reference that code so it'll be compiled.  To reference
-// this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
 import Vue from 'vue'
 
 import VueRouter from 'vue-router'
-Vue.use(VueRouter)
 import routes from 'routes'
-
-import 'normalize.css'
-import 'stylesheets/fonts.scss'
-import 'stylesheets/common.sass'
+Vue.use(VueRouter)
+const router = new VueRouter({routes, mode: 'history'})
 
 // add interceptor to add CSRF tokens to request
 import axios from 'axios'
@@ -32,10 +30,15 @@ axios.interceptors.request.use(function(config) {
   return config
 })
 
+import Layout from 'views/layout.vue'
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-            el: '#app',
-            router: new VueRouter({routes, mode: 'history'})
-          })
+    render: create => create(Layout),
+    router
+  }).$mount('#app');
 })
+
+import 'normalize.css'
+import 'stylesheets/fonts.scss'
+import 'stylesheets/common.sass'
